164e6f67acf8b26ff8c4c63e40588d7a99d789d7
Louie Weng <louieweng@outlook.com>
2023-02-23 22:34:47 -0800

    Project bootstrap from XCode

---
+++ Werk/WerkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerkApp.swift
+//  Werk
+//
+//  Created by Louie Weng on 2023-02-23.
+//
+
+import SwiftUI
+
+@main
+struct WerkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
